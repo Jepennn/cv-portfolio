550134b1bbc5c7416bce8b1a8ca7357ca4fd747f
Jesper Sandberg Hesselgren <jespersh@kth.se>
2024-12-18 00:26:56 +0100

    fixed miss in text

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -12,7 +12,7 @@ export function Projects() {
       <Box textAlign="center" mb={10} mt={5} mx="auto" w={["90%", "70%"]}>
         <Heading color={"grey.400"} size="2xl" fontWeight="medium" mb={5}>
           Here are some of the projects I have been working on. Click on the
-          buttons to get to the repositories or live app.Be aware that some of
+          buttons to get to the repositories or live app. Be aware that some of
           the live apps are slow to load since free tier.
         </Heading>
       </Box>
